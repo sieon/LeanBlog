af3499e9ba456de404f8899b204ba5eeef3fd80c
sieon <sieon@outlook.com>
2018-01-01 21:37:11 +0800

    User can upload topic image

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,6 +10,8 @@ use App\Models\Post;
 use App\Models\Category;
 use App\Models\Tag;
 
+use App\Handlers\ImageUploadHandler;
+
 use Auth;
 
 class PostsController extends Controller
@@ -67,4 +69,26 @@ class PostsController extends Controller
 
 		return redirect()->route('posts.index')->with('message', 'Deleted successfully.');
 	}
+
+    public function uploadImage(PostRequest $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据，默认是失败的
+        $data = [
+            'success'   => false,
+            'msg'       => '上传失败!',
+            'file_path' => ''
+        ];
+        // 判断是否有上传文件，并赋值给 $file
+        if ($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($request->upload_file, 'posts', \Auth::id(), 1024);
+            // 图片保存成功的话
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg']       = "上传成功!";
+                $data['success']   = true;
+            }
+        }
+        return $data;
+    }
 }
