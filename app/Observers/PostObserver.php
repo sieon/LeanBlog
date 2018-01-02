41b761883dd100f63ffa22d38cbcd9edb3971679
mxy <sieon@msn.cn>
2018-01-02 22:25:55 +0800

    Add queue for translate slug

--- app/Observers/PostObserver.php
+++ app/Observers/PostObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Post;
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -28,9 +29,15 @@ class PostObserver
         // 生成话题摘录
         $post->excerpt = make_excerpt($post->content);
 
+    }
+
+    public function saved(Post $post)
+    {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if ( ! $post->slug) {
-            $post->slug = app(SlugTranslateHandler::class)->translate($post->title);
+            //$post->slug = app(SlugTranslateHandler::class)->translate($post->title);
+            // 推送任务到队列
+            dispatch(new TranslateSlug($post));
         }
     }
 }
