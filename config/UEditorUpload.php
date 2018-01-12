b26842c7e0b8662b5f4dfe32570288f3d5056ac9
sieon <sieon@outlook.com>
2018-01-12 23:15:33 +0800

    baidu-u-editor-upload and rm markdown

--- config/UEditorUpload.php
+++ config/UEditorUpload.php
@@ -9,9 +9,9 @@ return [
 | 新增配置,route
 |--------------------------------------------------------------------------
 |
-|注意权限验证,请自行添加middleware 
+|注意权限验证,请自行添加middleware
 |middleware 相当重要,请根据自己的项目设置,比如如果在后台使用,请设置为后台的auth middleware.
-|如果是单纯本机测试,请将 
+|如果是单纯本机测试,请将
 |`// 'middleware' => 'auth',` 直接注释掉,如果留 `'middleware'=>''`空值,会产生bug,原因不详.
 |
 |
@@ -46,7 +46,7 @@ return [
         "imageCompressBorder" => 1600, /* 图片压缩最长边限制 */
         "imageInsertAlign" => "none", /* 插入的图片浮动方式 */
         "imageUrlPrefix" => "", /* 图片访问路径前缀 */
-        "imagePathFormat" => "/uploads/ueditor/php/upload/image/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
+        "imagePathFormat" => "/uploads/ueditor/images/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
         /* {filename} 会替换成原文件名,配置这项需要注意中文乱码问题 */
         /* {rand:6} 会替换成随机数,后面的数字是随机数的位数 */
         /* {time} 会替换成时间戳 */
@@ -63,14 +63,14 @@ return [
         /* 涂鸦图片上传配置项 */
         "scrawlActionName" => "uploadscrawl", /* 执行上传涂鸦的action名称 */
         "scrawlFieldName" => "upfile", /* 提交的图片表单名称 */
-        "scrawlPathFormat" => "/uploads/ueditor/php/upload/image/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
+        "scrawlPathFormat" => "/uploads/ueditor/images/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
         "scrawlMaxSize" => 2048000, /* 上传大小限制，单位B */
         "scrawlUrlPrefix" => "", /* 图片访问路径前缀 */
         "scrawlInsertAlign" => "none",
 
         /* 截图工具上传 */
         "snapscreenActionName" => "uploadimage", /* 执行上传截图的action名称 */
-        "snapscreenPathFormat" => "/uploads/ueditor/php/upload/image/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
+        "snapscreenPathFormat" => "/uploads/ueditor/images/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
         "snapscreenUrlPrefix" => "", /* 图片访问路径前缀 */
         "snapscreenInsertAlign" => "none", /* 插入的图片浮动方式 */
 
@@ -78,7 +78,7 @@ return [
         "catcherLocalDomain" => ["127.0.0.1", "localhost", "img.baidu.com"],
         "catcherActionName" => "catchimage", /* 执行抓取远程图片的action名称 */
         "catcherFieldName" => "source", /* 提交的图片列表表单名称 */
-        "catcherPathFormat" => "/uploads/ueditor/php/upload/image/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
+        "catcherPathFormat" => "/uploads/ueditor/images/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
         "catcherUrlPrefix" => "", /* 图片访问路径前缀 */
         "catcherMaxSize" => 2048000, /* 上传大小限制，单位B */
         "catcherAllowFiles" => [".png", ".jpg", ".jpeg", ".gif", ".bmp"], /* 抓取图片格式显示 */
@@ -86,7 +86,7 @@ return [
         /* 上传视频配置 */
         "videoActionName" => "uploadvideo", /* 执行上传视频的action名称 */
         "videoFieldName" => "upfile", /* 提交的视频表单名称 */
-        "videoPathFormat" => "/uploads/ueditor/php/upload/video/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
+        "videoPathFormat" => "/uploads/ueditor/videos/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
         "videoUrlPrefix" => "", /* 视频访问路径前缀 */
         "videoMaxSize" => 102400000, /* 上传大小限制，单位B，默认100MB */
         "videoAllowFiles" => [
@@ -96,7 +96,7 @@ return [
         /* 上传文件配置 */
         "fileActionName" => "uploadfile", /* controller里,执行上传视频的action名称 */
         "fileFieldName" => "upfile", /* 提交的文件表单名称 */
-        "filePathFormat" => "/uploads/ueditor/php/upload/file/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
+        "filePathFormat" => "/uploads/ueditor/files/{yyyy}{mm}{dd}/{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
         "fileUrlPrefix" => "", /* 文件访问路径前缀 */
         "fileMaxSize" => 51200000, /* 上传大小限制，单位B，默认50MB */
         "fileAllowFiles" => [
@@ -109,7 +109,7 @@ return [
 
         /* 列出指定目录下的图片 */
         "imageManagerActionName" => "listimage", /* 执行图片管理的action名称 */
-        "imageManagerListPath" => "/uploads/ueditor/php/upload/image/", /* 指定要列出图片的目录 */
+        "imageManagerListPath" => "/uploads/ueditor/images/", /* 指定要列出图片的目录 */
         "imageManagerListSize" => 20, /* 每次列出文件数量 */
         "imageManagerUrlPrefix" => "", /* 图片访问路径前缀 */
         "imageManagerInsertAlign" => "none", /* 插入的图片浮动方式 */
@@ -117,7 +117,7 @@ return [
 
         /* 列出指定目录下的文件 */
         "fileManagerActionName" => "listfile", /* 执行文件管理的action名称 */
-        "fileManagerListPath" => "/uploads/ueditor/php/upload/file/", /* 指定要列出文件的目录 */
+        "fileManagerListPath" => "/uploads/ueditor/files/", /* 指定要列出文件的目录 */
         "fileManagerUrlPrefix" => "", /* 文件访问路径前缀 */
         "fileManagerListSize" => 20, /* 每次列出文件数量 */
         "fileManagerAllowFiles" => [
